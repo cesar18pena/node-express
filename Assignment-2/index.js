7f80c8e855e852fe3cc8a88234e99f55b132a3ab
cesar18pena <cesar18pena@gmail.com>
2019-07-12 10:07:34 -0400

    Feat: continue with mongoose - part 2

--- Assignment-2/index.js
+++ Assignment-2/index.js
@@ -7,24 +7,22 @@ const connect = mongoose.connect(url);
 connect.then(() => {
   console.log('Successfully connected to the database');
 
-  let newDish = Dishes({
-    name: 'Uthappizza',
-    description: 'test'
-  });
-
-  newDish.save()
-      .then((dish) => {
-          console.log(dish);
-          return Dishes.find({});
-      })
-      .then((dishes) => {
-          console.log(dishes);
-          return Dishes.remove({});
-      })
-      .then(() => {
-          return mongoose.connection.close();
-      })
-      .catch((err) => {
-          console.log(err);
-      });
+  Dishes.create({
+    name: 'Uthapizza',
+    description: 'Test'
+    })
+    .then((dish) => {
+        console.log(dish);
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 })
