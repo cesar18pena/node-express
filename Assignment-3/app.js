65827a02a2fa57c5a1936fe1a2c482d8c760e820
cesar18pena <cesar18pena@gmail.com>
2019-07-14 15:06:13 -0400

    Feat: using cookie parser middleware

--- Assignment-3/app.js
+++ Assignment-3/app.js
@@ -28,30 +28,46 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-function auth(req, res, next) {
-  console.log(req.headers);
+function auth (req, res, next) {
 
-  let authHeader = req.headers.authorization;
-  if(!authHeader) {
-    const err = new Error('You are not authenticated!');
+  if (!req.signedCookies.user) {
+    const authHeader = req.headers.authorization;
 
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
+    if (!authHeader) {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');              
+      err.status = 401;
+      next(err);
+      return;
+    }
+
+    const auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    const user = auth[0];
+    const pass = auth[1];
+
+    if (user == 'admin' && pass == 'password') {
+      res.cookie('user','admin',{signed: true});
+      next(); // authorized
+    } else {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');              
+      err.status = 401;
+      next(err);
+    }
   }
-
-  const auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-
-  const username = auth[0];
-  const password = auth[1];
-
-  if(username === 'admin' && password === 'password'){
-    next();
+  else {
+    if (req.signedCookies.user === 'admin') {
+      next();
+    }
+    else {
+      const err = new Error('You are not authenticated!');
+      err.status = 401;
+      next(err);
+    }
   }
-
 }
 
 app.use(auth);
